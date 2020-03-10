26177a9454c55460ae607c30b0abd8bc2c06c90e
kentay <mathenge362@gmail.com>
2020-03-10 15:33:59 +0300

    Implement the search bar

--- snm/carowners/views.py
+++ snm/carowners/views.py
@@ -1,4 +1,5 @@
 """Views."""
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.core import serializers
@@ -7,6 +8,14 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, UpdateView
 
 from .models import CarModel, CarOwner
+
+
+class SignUp(CreateView):
+    """Sign up view for all users."""
+
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
 
 
 class CarownerCreateView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
