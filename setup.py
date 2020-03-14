0b19cbe209dae3c0f15a1f83dbfaa41395282984
kentay <mathenge362@gmail.com>
2020-03-14 11:48:06 +0300

    Deploy to pypi

---
+++ setup.py
@@ -0,0 +1,49 @@
+"""Sparesnmechs Python package setup."""
+from setuptools import setup
+
+
+name = "sparesnmechs"
+
+setup(
+    name=name,
+    version="0.0.1",
+    description="Sparesnmechs project",
+    long_description=open("README.rst").read(),
+    url="https://pypi.org/manage/projects/{}/".format(name),
+    author="Kenneth Mathenge",
+    author_email="mathenge362@gmail.com",
+    license="MIT License",
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
+    install_requires=[
+        "django~=2.2.10",
+        "psycopg2~=2.8.4",
+        "django-environ~=0.4.5",
+        "pytest-django~=3.7.0",
+        "pytest-cov~=2.8.1",
+        "tox~=3.14.1",
+        "Pillow~=6.2.1",
+        "model-bakery~=1.0.2",
+        "django-crispy-forms~=1.8.1",
+        "flake8~=3.7.9",
+        "django-filter~=2.2.0",
+        "gunicorn~=20.0.4",
+        "django-heroku~=0.3.1",
+        "wheel~=0.34.2",
+        "twine~=3.1.1",
+    ],
+    scripts=["bin/snm_manage"],
+    include_package_data=True,
+)
