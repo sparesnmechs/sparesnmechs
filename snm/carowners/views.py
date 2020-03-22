e8e99b329c948aac0a0df703d4085ae9644753c8
Mathenge <mathenge362@gmail.com>
2020-03-22 18:35:52 +0300

    Contact us page

--- snm/carowners/views.py
+++ snm/carowners/views.py
@@ -5,7 +5,7 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.core import serializers
 from django.http import HttpResponse
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.views.generic import CreateView, DetailView, UpdateView, TemplateView
 
 from .models import CarModel, CarOwner
 
@@ -16,6 +16,12 @@ class SignUp(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+
+class ContactUs(TemplateView):
+    """Create a contact us page."""
+
+    template_name = 'contact_us.html'
 
 
 class CarownerCreateView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
